e63108b4cd1bb0d988279c8a3692517660c08ccf
Roberto Toribio <roberttoribio04@gmail.com>
2023-09-15 16:41:22 -0700

    added hover to img

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,26 @@ const App = ()=>{
   //function that sets the value of the state variable count
   //to its current value plus multiplier when called.
   const updateCount = () => setCount(count + multiplier);
+  
+  const buyDoubleStuffed = () =>{
+    if (count >=10){
+      setCount(count-10)
+      setMultiplier(multiplier*2);
+    }
+  }
 
+  const buyPartyPack= () =>{
+    if(count>=100){
+      setMultiplier(multiplier*5);
+      setCount(count -100);
+    }
+  }
+  const buyFullFeast = () =>{
+    if(count>=1000){
+      setMultiplier(multiplier *10);
+      setCount(count - 1000)
+    }
+  }
   return (
     <div className="App">
       <div className="header">
@@ -27,19 +46,19 @@ const App = ()=>{
         <div className="upgrade">
           <h3>Double Stuffed 👯‍♀️</h3>
           <p> 2X per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
 
         <div className="upgrade">
           <h3>Party Pack 🎉</h3>
           <p>5X per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
 
         <div className="upgrade">
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10X per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
      
