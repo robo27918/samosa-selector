9775edd4a2cc8177a165969cd308501c5362d40f
Roberto Toribio <roberttoribio04@gmail.com>
2023-09-15 12:41:57 -0700

    Finished step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
-
+const imgLink = "https://assets.telegraphindia.com/telegraph/2021/Jan/1610742405_shutterstock_1348133177.jpg"
 const App = ()=>{
   return (
     <div className="App">
-      Hello world
+    <div className="header">
+      <h1>Samosa Selector</h1>
+      <h2>Count:</h2>
+      <img className='samosa' src ={imgLink}/>
     </div>
-  )
+     
+    </div>
+  );
 }
 export default App
